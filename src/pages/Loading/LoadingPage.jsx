5f8e1f65fe516c1ee780bad3041f919cce2a392a
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-05-15 11:34:05 -0700

    x

--- src/pages/Loading/LoadingPage.jsx
+++ src/pages/Loading/LoadingPage.jsx
@@ -5,7 +5,7 @@ export const LoadingPage = () => {
     <div className="container w-full ">
       <div className="loading absolute top-[40%] translate-y-[-50%] left-[47%] translate-x-[-50%]">
         <div className="logo">
-          <img src="/images/logo.png" alt="logo" />
+          <img src="images/logo.png" alt="logo" />
         </div>
         <h2 className="dark:text-popover">Todoo</h2>
         <div className="loading-progress">
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/todo-app/",
+  base: "/todo-app/",
   plugins: [react()],
   resolve: {
     alias: {
