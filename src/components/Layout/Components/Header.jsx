d679f91e2620e7c8b79263f492a31ed7928b759f
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-05-15 10:53:46 -0700

    commit

--- src/components/Layout/Components/Header.jsx
+++ src/components/Layout/Components/Header.jsx
@@ -1,11 +1,7 @@
 export const Header = () => {
   return (
     <div className="logo flex items-center pb-2 w-full">
-      <img
-        src="../../../../public/images/logo-h.png"
-        alt=""
-        className="w-full max-w-8"
-      />
+      <img src="/images/logo-h.png" alt="" className="w-full max-w-8" />
       <h2 className="inline font-bold mt-1 ml-3 dark:text-popover">Todoo</h2>
     </div>
   );
