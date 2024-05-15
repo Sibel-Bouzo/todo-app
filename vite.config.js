44acc4d061e4ae95c4084d2d5b9a12b11c2a52e4
Sibel Bouzo <bouzo.sibel@gmail.com>
2024-05-15 11:11:16 -0700

    c

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
